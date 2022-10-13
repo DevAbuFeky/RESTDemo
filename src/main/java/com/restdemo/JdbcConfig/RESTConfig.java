6edc6fd3c9be873156041697ba0604a28b739f15
Ahmed M. Elfeky <AhmedElFikii@science.menofia.edu.eg>
2022-10-13 11:45:39 +0200

    sec push

--- src/main/java/com/restdemo/JdbcConfig/RESTConfig.java
+++ src/main/java/com/restdemo/JdbcConfig/RESTConfig.java
@@ -4,7 +4,9 @@ import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
 import java.util.logging.Logger;
@@ -12,4 +14,14 @@ import java.util.logging.Logger;
 @Configuration
 @EnableWebMvc
 public class RESTConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/resources/templates/");
+        viewResolver.setSuffix("/");
+
+        return viewResolver;
+    }
 }
---
+++ src/main/java/com/restdemo/config/RESTDemoSecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.restdemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class RESTDemoSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
